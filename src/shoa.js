069a6e16c6508a169f8acf4739c6475fa979a5e1
Yotam Barzilay <yotamb@wix.com>
2016-12-20 10:34:20 +0200

    coverage over all files in src

--- src/shoa.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash');
-
-module.exports = {
-    shoa: _.constant(1)
-}
---
+++ test/example.test.js
@@ -0,0 +1,6 @@
+const test = require('tape');
+
+test('shoa', (t) => {
+    t.equal(1, 1, 'should be 1');
+    t.end();
+});
