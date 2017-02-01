846dc2925d0070d36ce1527eca2309cd249a829d
Yotam Barzilay <yotamb@wix.com>
2017-02-01 10:07:14 +0200

    lint

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,50 +1,52 @@
-  $(function () {
+/* global _, $, firebase*/
+/* eslint-disable no-var */
+$(function () {
 
-  var first = [];
-  var second = [];
+	var first = [];
+	var second = [];
 
-  function getWordsFromDB(cb) {
-    firebase.database().ref('words').once('value', function (snapshot) {
-        cb(snapshot.val())
-    });
-  }
+	function getWordsFromDB(cb) {
+		firebase.database().ref('words').once('value', function (snapshot) {
+			cb(snapshot.val());
+		});
+	}
 
-  function setWords(words) {
-    first = _.values(words.first);
-    second = _.values(words.second);
-  }
+	function setWords(words) {
+		first = _.values(words.first);
+		second = _.values(words.second);
+	}
 
-    function generateCouple() {
-      var couple = {
-        first: _.sample(first),
-        second: _.sample(second)
-      };
+	function generateCouple() {
+		var couple = {
+			first: _.sample(first),
+			second: _.sample(second)
+		};
 
-      $(".first, .second").fadeOut("fast", function () {
-        $(".first").html(couple.first);
-        $(".second").html(couple.second);
+		$('.first, .second').fadeOut('fast', function () {
+			$('.first').html(couple.first);
+			$('.second').html(couple.second);
 
-        $(".first, .second").fadeIn("fast");
-      })
-    }
+			$('.first, .second').fadeIn('fast');
+		});
+	}
 
-    function transitionIntoView() {
-      $("#loading")
-        .fadeOut(100, function () {
-          $("#contentWrapper").fadeIn(300);
-        })
-    }
+	function transitionIntoView() {
+		$('#loading')
+			.fadeOut(100, function () {
+				$('#contentWrapper').fadeIn(300);
+			});
+	}
 
-    function init() {
-      getWordsFromDB(function (words) {
-          setWords(words);
-          generateCouple();
+	function init() {
+		getWordsFromDB(function (words) {
+			setWords(words);
+			generateCouple();
 
-          $(".generate-button").on('click', generateCouple);
+			$('.generate-button').on('click', generateCouple);
 
-          transitionIntoView();
-      });
-    }
+			transitionIntoView();
+		});
+	}
 
-    init();
+	init();
 });
